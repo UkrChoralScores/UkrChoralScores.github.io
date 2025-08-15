536e3d6d5c06a8e6c7bb1312e267c04ee1eab61e
UkrChoralScores <ukrchoralscores@gmail.com>
2025-08-15 22:18:27 +0300

    Delete search.js

--- search.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('search').addEventListener('input', function() {
-    let filter = this.value.toLowerCase();
-    let scores = document.querySelectorAll('#score-list .score-card');
-    scores.forEach(score => {
-        let text = score.textContent.toLowerCase();
-        score.style.display = text.includes(filter) ? '' : 'none';
-    });
-});
